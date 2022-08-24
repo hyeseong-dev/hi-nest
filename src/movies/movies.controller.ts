504d783a30ea24870beb4fc8b8e3d881eb28683f
이혜성 <main@hyeseong.local>
2022-08-24 22:32:13 +0900

    more routers Param decorators
    ```
    // NestJS
    @Body(param?: string)
    
    // ExpressJS
    req.body / req.body[param]
    
    // NestJS
    @Query(param?: string)
    
    // ExpressJS
    req.query / req.query[param]
    ```
    https://docs.nestjs.com/custom-decorators\#param-decorators

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,9 +7,20 @@ export class MoviesController {
     return `Get All Movies`;
   }
 
+  @Get(`search`)
+  search(@Query(`year`) searchingYear: string) {
+    return `we are searching the title of the movie made after ${searchingYear}`;
+  }
+
   @Get(`/:id`)
   getOne(@Param(`id`) id: string) {
     return `One Movie ${id}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    console.log(movieData);
+    return movieData;
   }
 
   @Delete(`/:id`)
@@ -18,7 +29,10 @@ export class MoviesController {
   }
 
   @Patch(`/:id`)
-  patch(@Param(`id`) movieId: string) {
-    return `This will update  movie with the id: ${movieId}`;
+  patch(@Param(`id`) movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
