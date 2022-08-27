89963335f648b6033923fe45f264fab68a38e7f6
이혜성 <main@hyeseong.local>
2022-08-28 05:39:01 +0900

    testing update'

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -76,4 +76,23 @@ describe('MoviesService', () => {
       expect(afterCreate).toBeGreaterThan(beforeCreate);
     });
   });
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.create({
+        title: 'test movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      service.update(1, { title: 'Updated Test' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+    it('should throw a NotFoundException if the movie does not exist', () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
 });
